2db4a484fc265e71c800315bdb521f775da85682
good665 <good66591@gmail.com>
2021-06-20 06:15:21 +0900

    tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,6 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE':'django.db.backends.mysql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
